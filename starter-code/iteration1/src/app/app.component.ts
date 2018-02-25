42cabbb2557b88368d91ae8b52d1165ff4cd5c6b
Santi Ramos <santi92.network@gmail.com>
2018-02-25 23:33:40 +0100

    iteration 1 complete

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
+function clean(target) {
+  target.prototype.cleaned = true;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+@clean
 export class AppComponent {
-  title = 'app works!';
+  title = 'Iteration 1';
+  animals: Array<Object> = [
+    {
+      id: 1,
+      category: 'mammal',
+      name: 'Dog'
+    }, {
+      id: 2,
+      category: 'oviparous',
+      name: 'Duck'
+    }, {
+      id: 3,
+      category: 'mammal',
+      name: 'Elephant'
+    }, {
+      id: 4,
+      category: 'reptile',
+      name: 'Snake'
+    }
+  ];
+  // img : string = "https://significadodelossuenoss.com/wp-content/uploads/2015/12/significado-de-sonar-con-gatitos.jpg";
 }
